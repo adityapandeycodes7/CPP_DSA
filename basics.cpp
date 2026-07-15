0e5332f4c8d3b518d90893397462755d8eb42581
adityapandeycodes7 <adityapandey2937@gmail.com>
2026-07-14 23:40:59 -0700

    Adding more Pattern questions

--- basics.cpp
+++ basics.cpp
@@ -389,20 +389,42 @@ int main () {
 
 
 //----------> Inverted Triangle Pattern:
+// int n;
+// cout<<"Enter the value of n: ";
+// cin>> n;
+
+// for(int i=0; i<n; i++){
+//     int count = 1;
+//     for(int m=0; m<i; m++){
+//         cout<<" ";
+//     }
+//     for(int j=0; j<n-i; j++){
+//         cout<< i+1;
+//     }
+//     cout<<endl;
+// }
+
+
+
+
+
+//------------> Pyramid Pattern:
+
 int n;
 cout<<"Enter the value of n: ";
 cin>> n;
-
 for(int i=0; i<n; i++){
-    int count = 1;
-    for(int m=0; m<i; m++){
-        cout<<" ";
-    }
-    for(int j=0; j<n-i; j++){
-        cout<< i+1;
-    }
-    cout<<endl;
-}
 
+for(int j=0; j<n-i-1; j++){
+    cout<<" ";
+}
+    for(int j=1; j<=i+1; j++){
+        cout<< j;
+    }
+    for(int j=0; j>0; j--){
+    cout<< j;
+    }
+    cout<< endl;
+}
 return 0;
 }
