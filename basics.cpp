dae8c6acce316bdd5c6318f06db2a324e8b4b636
adityapandeycodes7 <adityapandey2937@gmail.com>
2026-08-28 04:55:25 -0700

    Added Butterfly Pattern Question

--- basics.cpp
+++ basics.cpp
@@ -436,35 +436,68 @@ int main () {
 
 
 
+// int n = 4;
+//  for(int i = 0; i<n; i++){
+//     for(int j = 0; j < n-i-1; j++){
+//         cout<<" ";
+//     }
+//     cout<<"*";
+//     if(i!=0){
+//         for(int k = 0; k<= 2*i-1-1; k++){
+//             cout<<" ";
+//         }
+//         cout<<"*";
+//     }
+//     cout<<endl;
+//  }
+
+//  for(int i = 0; i< n-1; i++){
+//     for(int j = 0; j < i+1; j++){
+//         cout<<" ";
+//     }
+//     cout<<"*";
+//     if(i!=n-2){
+//         for(int k=0; k < 2*(n-2-i)-1; k++){
+//             cout<<" ";
+//         }
+//         cout<<"*";
+//     }
+//     cout<<endl;
+//  }
+
+
+
+
+//------------------> Butterfly Pattern:
+
 int n = 4;
- for(int i = 0; i<n; i++){
-    for(int j = 0; j < n-i-1; j++){
+
+for(int i = 0; i<n; i++){
+    for(int j=0; j<=i; j++){
+        cout<<"*";
+    }
+    for(int j = 0; j < (2*n)-2-(2*i); j++){
         cout<<" ";
     }
-    cout<<"*";
-    if(i!=0){
-        for(int k = 0; k<= 2*i-1-1; k++){
-            cout<<" ";
-        }
+    for(int j=0; j<=i; j++){
         cout<<"*";
     }
     cout<<endl;
- }
+}
 
- for(int i = 0; i< n-1; i++){
-    for(int j = 0; j < i+1; j++){
+for(int i = 0; i<n; i++){
+    for(int j = 0; j<n-i; j++){
+        cout<<"*";
+    }
+
+    for(int j = 0; j<2*i; j++){
         cout<<" ";
     }
-    cout<<"*";
-    if(i!=n-2){
-        for(int k=0; k < 2*(n-2-i)-1; k++){
-            cout<<" ";
-        }
+    for(int j = 0; j<n-i; j++){
         cout<<"*";
     }
     cout<<endl;
- }
-
+}
 
 return 0;
 }
