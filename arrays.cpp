dd76e3d6d5c2d1ce2df300832988b73a5dbb1f7b
adityapandeycodes7 <adityapandey2937@gmail.com>
2026-09-14 11:27:20 -0700

    correction

--- arrays.cpp
+++ arrays.cpp
@@ -327,12 +327,6 @@ vector<int>ans(size,1);
 vector<int>prefix(size,1);
 vector<int>suffix(size,1);
 
-// prefix[0] = 1;
-// suffix[size - 1] = 1;
-
-// int prefProd = 1;
-// int suffProd = 1;
-// int ansProd = 1;
 for(int i = 1; i<size; i++){ // prefix Arr
 
   prefix[i] = prefix[i-1] * nums[i-1];
@@ -342,7 +336,7 @@ for(int i = size-2; i>=0; i--){ // suffix Arr
 suffix[i] = suffix[i+1] * nums[i+1];
 }
 
-for(int i = 0; i<size; i++){
+for(int i = 0; i<size; i++){ // ans Arr
   ans[i] =  prefix[i] * suffix[i];
   cout<<ans[i]<<" ";
 }
