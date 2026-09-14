53e318555fd8f3e1d16e46fc2c9e732322fc1554
adityapandeycodes7 <adityapandey2937@gmail.com>
2026-09-14 11:44:13 -0700

    (Leetcode 238) Optimizing Space Complexity

--- arrays.cpp
+++ arrays.cpp
@@ -321,24 +321,47 @@ int main () {
 
 
 //-------> Optimal Approach: (Time Complexity = O(n)):
+// vector<int>nums = {1,2,3,4};
+// int size = nums.size();
+// vector<int>ans(size,1);
+// vector<int>prefix(size,1);
+// vector<int>suffix(size,1);
+
+// for(int i = 1; i<size; i++){ // prefix Arr
+
+//   prefix[i] = prefix[i-1] * nums[i-1];
+// }
+
+// for(int i = size-2; i>=0; i--){ // suffix Arr
+// suffix[i] = suffix[i+1] * nums[i+1];
+// }
+
+// for(int i = 0; i<size; i++){ // ans Arr
+//   ans[i] =  prefix[i] * suffix[i];
+//   cout<<ans[i]<<" ";
+// }
+
+
+
+//-------> Optimizing the space Complexity: O(1):
 vector<int>nums = {1,2,3,4};
 int size = nums.size();
 vector<int>ans(size,1);
-vector<int>prefix(size,1);
-vector<int>suffix(size,1);
 
-for(int i = 1; i<size; i++){ // prefix Arr
-
-  prefix[i] = prefix[i-1] * nums[i-1];
+ for(int i = 1; i<size; i++){ // prefix Arr
+  ans[i] = ans[i-1] * nums[i-1];
 }
 
-for(int i = size-2; i>=0; i--){ // suffix Arr
-suffix[i] = suffix[i+1] * nums[i+1];
+int suffix = 1;
+
+ for(int i = size-2; i>=0; i--){ // suffix Arr
+     suffix *= nums[i+1];
+  ans[i] *= suffix;
 }
 
 for(int i = 0; i<size; i++){ // ans Arr
-  ans[i] =  prefix[i] * suffix[i];
   cout<<ans[i]<<" ";
 }
+
 return 0;
 }
