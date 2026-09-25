2850c0ed226415dea6c7774e63a250b96479f2ad
adityapandeycodes7 <adityapandey2937@gmail.com>
2026-09-25 09:21:26 -0700

    Overflow Prevention and correction

--- arrays.cpp
+++ arrays.cpp
@@ -474,31 +474,49 @@ int main () {
 
 //--------------> Single Element in Sorted Array: (Leetcode 540):
 
-vector<int>nums = {1,1,2,3,3,4,4,8,8};
+vector<int>nums = {3,3,7,7,10,11,11};
 int size = nums.size();
 int start = 0;
 int end = size - 1;
 
+if(size == 1){
+    cout<<nums[0];
+    return 0;
+}
 while(start <= end){
     int mid = start + (end - start)/2;
-    
+
+    if(mid == 0 && nums[mid] != nums[mid + 1]){
+        cout<<nums[mid];
+        break;
+    }
+
+    if(mid == size-1 && nums[mid] != nums[mid - 1]){
+        cout<<nums[mid];
+        break;
+    }
     if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]){
         cout<< nums[mid]<<endl;
         break;
     }
-    else if( mid % 2 == 0 && nums[mid] == nums[mid - 1]){
-        end = mid - 1;
+
+    if( mid % 2 == 0){
+        if(nums[mid] == nums[mid - 1]){
+            end = mid - 1;
+        }
+
+        else{
+            start = mid + 1;
+        }
     }
-    else if( mid % 2 == 0 && nums[mid] == nums[mid + 1]){
-        start = mid + 1;
-    }
-    else if( mid % 2 != 0 && nums[mid] == nums[mid - 1]){
+    else{ 
+    if(nums[mid] == nums[mid - 1]){
         start = mid + 1;
     }
     else{
         end = mid - 1;
     }
 }
-
+}
 return 0;
 }
