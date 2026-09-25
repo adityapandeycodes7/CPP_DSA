71d27a25baffc60dfc54207018c883b86d79313d
adityapandeycodes7 <adityapandey2937@gmail.com>
2026-09-24 23:11:15 -0700

    Single Element in Sorted Array: (Leetcode 540)

--- arrays.cpp
+++ arrays.cpp
@@ -448,27 +448,57 @@ int main () {
 
 //--------->Optimised Solution: Time Complexity O(log n):
 
-vector<int>arr = {0,3,8,9,5,2};
-int size = arr.size();
-int start = 1;
-int end = size-2;
+// vector<int>arr = {0,3,8,9,5,2};
+// int size = arr.size();
+// int start = 1;
+// int end = size-2;
+
+// while(start <= end){
+//     int mid = start + (end - start)/2;
+    
+//     if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid + 1]){
+//         cout<< arr[mid];
+//         break;
+//     }
+
+//     else if(arr[mid] > arr[mid - 1]){
+//         start = mid + 1;
+//     }
+//     else{
+//         end = mid - 1;
+//         }
+// }
+
+
+
+
+//--------------> Single Element in Sorted Array: (Leetcode 540):
+
+vector<int>nums = {1,1,2,3,3,4,4,8,8};
+int size = nums.size();
+int start = 0;
+int end = size - 1;
 
 while(start <= end){
     int mid = start + (end - start)/2;
     
-    if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid + 1]){
-        cout<< arr[mid];
+    if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]){
+        cout<< nums[mid]<<endl;
         break;
     }
-
-    else if(arr[mid] > arr[mid - 1]){
+    else if( mid % 2 == 0 && nums[mid] == nums[mid - 1]){
+        end = mid - 1;
+    }
+    else if( mid % 2 == 0 && nums[mid] == nums[mid + 1]){
+        start = mid + 1;
+    }
+    else if( mid % 2 != 0 && nums[mid] == nums[mid - 1]){
         start = mid + 1;
     }
     else{
         end = mid - 1;
-        }
+    }
 }
-
 
 return 0;
 }
